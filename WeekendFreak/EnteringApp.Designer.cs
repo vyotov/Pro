35a804ecc16078f3f1e880cd5ff2c0e1e869cd2c
vyotov <ventsislav_yotov@yahoo.com>
2015-05-31 17:54:57 +0200

    EnteringApp Update

--- WeekendFreak/EnteringApp.Designer.cs
+++ WeekendFreak/EnteringApp.Designer.cs
@@ -57,7 +57,7 @@
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(104, 37);
             this.button6.TabIndex = 87;
-            this.button6.Text = "Activate";
+            this.button6.Text = "Check In";
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
